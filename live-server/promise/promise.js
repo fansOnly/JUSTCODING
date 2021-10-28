0f095a0377fba71a8295df886008e32f1e78e3d6
fansonly <251115151@qq.com>
2021-10-28 15:29:02 +0800

    promise 源码探秘

--- live-server/promise/promise.js
+++ live-server/promise/promise.js
@@ -5,11 +5,9 @@
 function Promise(executor) {
 
   // 状态
-  this.state = 'pending'
-  // 成功的返回值
-  this.value = null
-  // 失败的返回值
-  this.reason = null
+  this.PromiseState = 'pending'
+  // 成功 / 失败 的返回值
+  this.PromiseResult = null
   // 存储回调函数
   this.callbacks = []
   // 注意调用时的 this 指向，默认 window
@@ -22,9 +20,9 @@ function Promise(executor) {
    */
   function resolve(value) {
     // 确保状态只改变一次
-    if (self.state === 'pending') {
-      self.state = 'fulfilled'
-      self.value = value
+    if (self.PromiseState === 'pending') {
+      self.PromiseState = 'fulfilled'
+      self.PromiseResult = value
       self.callbacks.forEach(item => {
         item.onResolved(value)
       })
@@ -38,9 +36,9 @@ function Promise(executor) {
    */
   function reject(reason) {
     // 确保状态只改变一次
-    if (self.state === 'pending') {
-      self.state = 'rejected'
-      self.reason = reason
+    if (self.PromiseState === 'pending') {
+      self.PromiseState = 'rejected'
+      self.PromiseResult = reason
       self.callbacks.forEach(item => {
         item.onRejected(reason)
       })
@@ -62,13 +60,41 @@ function Promise(executor) {
  * @param {*} onRejected 响应失败的回调
  */
 Promise.prototype.then = function (onResolved, onRejected) {
-  const self = this
-  let promise
+  // 如果没有传入回调，默认输出
+  onResolved = typeof onResolved === 'function' ? onResolved : value => value
+  onRejected = typeof onRejected === 'function' ? onRejected : err => { throw err }
+  // then 方法默认返回一个 promise
   return new Promise((resolve, reject) => {
-    if (this.state === 'fulfilled') {
-      try {
-        // 回调函数的执行结果
-        promise = onResolved(this.value)
+    if (this.PromiseState === 'fulfilled') {
+      resolvePromise(onResolved, this.PromiseResult, resolve, reject)
+    } else if (this.PromiseState === 'rejected') {
+      resolvePromise(onRejected, this.PromiseResult, resolve, reject)
+    } else {
+      // 处理异步的状态改变
+      this.callbacks.push({
+        onResolved: () => {
+          resolvePromise(onResolved, this.PromiseResult, resolve, reject)
+        },
+        onRejected: () => {
+          resolvePromise(onRejected, this.PromiseResult, resolve, reject)
+        }
+      })
+    }
+  })
+}
+
+
+/**
+ * 处理 promise 返回的结果
+ * 1. 非 promise 值直接返回
+ * 2. promise 值返回相应的 promise 结果
+ */
+function resolvePromise(cb, value, resolve, reject) {
+  setTimeout(() => {
+    try {
+      // 回调函数的执行结果
+      // if (cb !== undefined) {
+        const promise = cb(value)
         if (promise instanceof Promise) {
           // promise 值需要执行 then 方法后返回
           promise.then(v => {
@@ -80,33 +106,95 @@ Promise.prototype.then = function (onResolved, onRejected) {
           // 非 promise 值直接返回
           resolve(promise)
         }
-      } catch (e) {
-        // 抛出异常直接返回失败状态的 promise
-        reject(e)
-      }
-    } else if (this.state === 'rejected') {
-      promise = onRejected(this.reason)
+      // } else {
+      //   resolve(cb)
+      // }
+    } catch (e) {
+      // 抛出异常直接返回失败状态的 promise
+      reject(e)
+    }
+  }, 0);
+}
+
+
+/**
+ * Promise.prototype.catch
+ */
+Promise.prototype.catch = function (onRejected) {
+  return this.then(undefined, onRejected)
+}
+
+
+/**
+ * Promise.resolve
+ */
+Promise.resolve = function (value) {
+  return new Promise((resolve, reject) => {
+    if (value instanceof Promise) {
+      value.then(v => {
+        resolve(v)
+      }, r => {
+        reject(r)
+      })
     } else {
-      // 处理异步的状态改变
-      this.callbacks.push({
-        onResolved: function () {
-          promise = onResolved(self.value)
-        },
-        onRejected: function () {
-          promise = onResolved(self.reason)
+      resolve(value)
+    }
+  })
+}
+
+
+/**
+ * Promise.reject
+ */
+Promise.reject = function (reason) {
+  return new Promise((resolve, reject) => {
+    reject(reason)
+  })
+}
+
+
+/**
+ * promise.all
+ */
+Promise.all = function(promises) {
+  const len = promises.length
+  let count = 0
+  let result = []
+  return new Promise((resolve, reject) => {
+    for (let i = 0; i < len; i++) {
+      const promise = promises[i]
+      promise.then(value => {
+        // 不能用 push 推入，注意（异步） promise 状态改变的顺序
+        result[i] = value
+        if (++count === len) {
+          // 全部成功才返回成功状态
+          resolve(result)
         }
+      }, reason => {
+        // 返回失败
+        reject(reason)
       })
     }
   })
 }
 
 
-
-
-
-
-
-
+/**
+ * promise.race
+ */
+Promise.race = function(promises) {
+  return new Promise((resolve, reject) => {
+    for (let i = 0; i < promises.length; i++) {
+      promises[i].then(value => {
+        // 返回成功
+        resolve(value)
+      }, reason => {
+        // 返回失败
+        reject(reason)
+      })
+    }
+  })
+}
 
 
 
---
+++ live-server/promise/promiseClass.js
@@ -0,0 +1,168 @@
+class Promise {
+  constructor(executor) {
+    // 状态
+    this.PromiseState = 'pending'
+    // 成功 / 失败 的返回值
+    this.PromiseResult = null
+    // 存储回调函数
+    this.callbacks = []
+    // 注意调用时的 this 指向，默认 window
+    const self = this
+
+    /**
+     * 成功回调
+     * 1. 修改状态
+     * 2. 返回值
+     */
+    function resolve(value) {
+      // 确保状态只改变一次
+      if (self.PromiseState === 'pending') {
+        self.PromiseState = 'fulfilled'
+        self.PromiseResult = value
+        self.callbacks.forEach(item => {
+          item.onResolved(value)
+        })
+      }
+    }
+
+    /**
+     * 失败回调
+     * 1. 修改状态
+     * 2. 返回值
+     */
+    function reject(reason) {
+      // 确保状态只改变一次
+      if (self.PromiseState === 'pending') {
+        self.PromiseState = 'rejected'
+        self.PromiseResult = reason
+        self.callbacks.forEach(item => {
+          item.onRejected(reason)
+        })
+      }
+    }
+
+    try {
+      // 捕获通过 throw 抛出的异常
+      executor(resolve, reject)
+    } catch (e) {
+      // 修改 promise 的状态为失败
+      reject(e)
+    }
+  }
+
+  then(onResolved, onRejected) {
+    // 如果没有传入回调，默认输出
+    onResolved = typeof onResolved === 'function' ? onResolved : value => value
+    onRejected = typeof onRejected === 'function' ? onRejected : err => { throw err }
+    // then 方法默认返回一个 promise
+    return new Promise((resolve, reject) => {
+      if (this.PromiseState === 'fulfilled') {
+        resolvePromise(onResolved, this.PromiseResult, resolve, reject)
+      } else if (this.PromiseState === 'rejected') {
+        resolvePromise(onRejected, this.PromiseResult, resolve, reject)
+      } else {
+        // 处理异步的状态改变
+        this.callbacks.push({
+          onResolved: () => {
+            resolvePromise(onResolved, this.PromiseResult, resolve, reject)
+          },
+          onRejected: () => {
+            resolvePromise(onRejected, this.PromiseResult, resolve, reject)
+          }
+        })
+      }
+    })
+  }
+
+  catch(onRejected) {
+    return this.then(undefined, onRejected)
+  }
+
+  static resolve (value) {
+    return new Promise((resolve, reject) => {
+      if (value instanceof Promise) {
+        value.then(v => {
+          resolve(v)
+        }, r => {
+          reject(r)
+        })
+      } else {
+        resolve(value)
+      }
+    })
+  }
+
+  static reject (reason) {
+    return new Promise((resolve, reject) => {
+      reject(reason)
+    })
+  }
+
+  static all (promises) {
+    const len = promises.length
+    let count = 0
+    let result = []
+    return new Promise((resolve, reject) => {
+      for (let i = 0; i < len; i++) {
+        const promise = promises[i]
+        promise.then(value => {
+          // 不能用 push 推入，注意（异步） promise 状态改变的顺序
+          result[i] = value
+          if (++count === len) {
+            // 全部成功才返回成功状态
+            resolve(result)
+          }
+        }, reason => {
+          // 返回失败
+          reject(reason)
+        })
+      }
+    })
+  }
+
+  static race (promises) {
+    return new Promise((resolve, reject) => {
+      for (let i = 0; i < promises.length; i++) {
+        promises[i].then(value => {
+          // 返回成功
+          resolve(value)
+        }, reason => {
+          // 返回失败
+          reject(reason)
+        })
+      }
+    })
+  }
+}
+
+/**
+ * 处理 promise 返回的结果
+ * 1. 非 promise 值直接返回
+ * 2. promise 值返回相应的 promise 结果
+ */
+ function resolvePromise(cb, value, resolve, reject) {
+  setTimeout(() => {
+    try {
+      // 回调函数的执行结果
+      // if (cb !== undefined) {
+        const promise = cb(value)
+        if (promise instanceof Promise) {
+          // promise 值需要执行 then 方法后返回
+          promise.then(v => {
+            resolve(v)
+          }, r => {
+            reject(r)
+          })
+        } else {
+          // 非 promise 值直接返回
+          resolve(promise)
+        }
+      // } else {
+      //   resolve(cb)
+      // }
+    } catch (e) {
+      // 抛出异常直接返回失败状态的 promise
+      reject(e)
+    }
+  }, 0);
+}
