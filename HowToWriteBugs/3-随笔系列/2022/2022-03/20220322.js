a0c62805f2877e1f1dc0af5e26407a6a1ca8cee6
fansonly <251115151@qq.com>
2022-03-24 19:53:40 +0800

    没事写两行代码～～～

--- HowToWriteBugs/3-随笔系列/2022/2022-03/20220322.js
+++ HowToWriteBugs/3-随笔系列/2022/2022-03/20220322.js
@@ -2,7 +2,7 @@
  * @Author: fansonly
  * @Date: 2022-03-22 14:52:48
  * @Description:
- * @LastEditTime: 2022-03-22 14:59:06
+ * @LastEditTime: 2022-03-24 19:52:25
  */
 
 function moneyRule(value) {
@@ -18,3 +18,26 @@ function moneyRule2(value) {
 }
 
 console.log(moneyRule2('111.10'))
+
+
+export function checkPass(v) {
+  return /(?!.*\s)(?!^[\u4e00-\u9fa5]+$)(?!^[0-9]+$)(?!^[A-z]+$)(?!^[^A-z0-9]+$)^.{8,16}$/.test(String(v))
+}
+
+
+console.log(checkPass('111111111a'))
+
+
+
+export const dataMasking = (value, start = 0, end = 0) => {
+  value = String(value)
+  if (end > value.length) end = value.length
+  if (start < 0) start = 0
+  const reg = new RegExp('(\\d{' + start + '})\\d*(\\d{' + (value.length - end) + '})')
+  return value.replace(reg, (_, $1, $2) => {
+    return $1 + '*'.repeat(end - start).replace(/\B(?=(\*{4})+$)/g, ' ') + ' ' + $2
+  })
+}
+
+console.log(dataMasking('13344445555', 3, 7))
+console.log(dataMasking('133444455556666', 3, 11))
