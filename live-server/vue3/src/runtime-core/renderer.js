520197ad343f80fb086945d85492123bb92ff100
fansoly <251115151@qq.com>
2022-11-25 08:56:13 +0800

    feat: working on mini reactive system

--- live-server/vue3/src/runtime-core/renderer.js
+++ live-server/vue3/src/runtime-core/renderer.js
@@ -374,7 +374,6 @@ export function createRenderer(options) {
 
   const render = (vnode, container) => {
     if (vnode) {
-      console.log('containe', container._vnode || null, vnode);
       patch(container._vnode || null, vnode, container)
     } else {
       if (container._vnode) {
