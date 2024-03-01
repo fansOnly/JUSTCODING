25fd610a4ff2cb3ded05854f6eccc09db98abdb3
fansonly <251115151@qq.com>
2024-03-01 11:08:42 +0800

    2 月没了

---
+++ 2024/2024-02/20240228-29.js
@@ -0,0 +1,26 @@
+// const arr = [1,2,3,1,2,1,3]
+
+// const res = Array.from(new Set(arr))
+// console.log('res: ', res);
+
+
+/**
+ * 1. 请求行：url、method、协议
+ * 2. 请求头
+ *  2.1 Accept
+ *  2.2 Connection
+ *  2.3 content-type
+ *  2.4 Host
+ *  2.5 User-Agent
+ *  2.6 Referer
+ * 3. 请求体：请求参数
+ * 4. 空行
+ */
+
+
+/**
+ * 浏览器渲染
+ * 1. HTML -> [HTML Parser + DOM] -> DOM tree
+ * 2. Style Sheets -> CSS Parser -> Style Rules
+ * 3. [DOM tree + Style Rules] + Attachment -> [Render Tree + Layout] -> Painting -> Display
+ */
