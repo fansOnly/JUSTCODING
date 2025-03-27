429260ca29fe3b59247f548023a24810e1ef0bd6
fansonly <251115151@qq.com>
2025-03-27 10:11:16 +0800

    feat: python 文件操作

---
+++ python/file-operation.py
@@ -0,0 +1,24 @@
+import shutil
+import os
+# import send2trash
+
+# send2trash.send2trash('1.txt')
+
+# # create a new directory
+# os.mkdir('new_dir')
+
+# # copy a file to the new directory
+# shutil.copy('1.txt', 'new_dir')
+
+
+# shutil.copytree('new_dir', 'new_dir_copy')
+
+# shutil.move('new_dir_copy', 'new_dir')
+
+
+# os.unlink(os.path.join('new_dir', '1.txt'))
+
+# os.mkdir('new_dir')
+# os.chdir('new_dir')
+# print(os.getcwd())
+# os.rmdir('new_dir')
